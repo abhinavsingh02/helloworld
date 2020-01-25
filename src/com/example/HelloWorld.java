7048bf05703bee2344da5bdf4819129a7e412364
abhinavsingh02 <abhinav.singh02@nagarro.com>
2020-01-25 23:19:43 +0530

    hey

--- src/com/example/HelloWorld.java
+++ src/com/example/HelloWorld.java
@@ -3,8 +3,6 @@ package com.example;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello java world");
-		System.out.println("Learnt again");
-		System.out.println("Hey Jai Guru Dev");
 		
 	}
 }
