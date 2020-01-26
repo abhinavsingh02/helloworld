58f81dc02554ed4c806c0c714f752db15bbee7a7
abhinavsingh02 <abhinav.singh02@nagarro.com>
2020-01-26 12:47:28 +0530

    Deleted two output statements

--- src/com/example/HelloWorld.java
+++ src/com/example/HelloWorld.java
@@ -3,7 +3,6 @@ package com.example;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello java world");
-		System.out.println("code version 0.1");
-		System.out.println("This is commiting to HUB repository");
+		
 	}
 }
