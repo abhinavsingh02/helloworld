49475ef0d2399f4cd3314dbf39b3552067142f59
abhinavsingh02 <abhinav.singh02@nagarro.com>
2020-01-26 11:40:25 +0530

    Jai Guru Dev

--- src/com/example/HelloWorld.java
+++ src/com/example/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello java world");
 		System.out.println("code version 0.1");
-		System.out.println("This is commiting to local repository");
+		System.out.println("This is commiting to HUB repository");
 	}
 }
